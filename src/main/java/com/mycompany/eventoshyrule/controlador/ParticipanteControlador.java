e3542bff02571b2de7c94456988fe36b051df4c6
RonaldSamayoa <RonaldSamayoa@github.com>
2025-08-13 20:45:02 -0600

    controlador de participante

---
+++ src/main/java/com/mycompany/eventoshyrule/controlador/ParticipanteControlador.java
@@ -0,0 +1,70 @@
+package com.mycompany.eventoshyrule.controlador;
+import com.mycompany.eventoshyrule.dao.ParticipanteDAO;
+import com.mycompany.eventoshyrule.modelo.Participante;
+import java.util.List;
+
+/**
+ *
+ * @author ronald
+ */
+public class ParticipanteControlador {
+    private ParticipanteDAO participanteDAO;
+    
+    public ParticipanteControlador (){
+        this.participanteDAO = new ParticipanteDAO();
+    }
+    
+    //crear participante
+    public boolean crearParticipante(String correo, String nombreCompleto, String tipoParticipante, String institucion){
+        if (correo==null || correo.isEmpty()) {
+            System.out.println("El correo del participante no puede estar vacio");
+            return false;
+        }
+        
+        if (nombreCompleto==null || nombreCompleto.isEmpty()) {
+            System.out.println("El nombre del participante no puede estar vacio");
+            return false;
+        }
+        
+        if (tipoParticipante==null || tipoParticipante.isEmpty()) {
+            System.out.println("Debe indicar el tipo de participante");
+            return false;
+        }
+        
+        Participante participante = new Participante (correo, nombreCompleto, tipoParticipante, institucion);
+        return participanteDAO.insertar(participante);
+    }
+    
+    //listar a los participantes
+    public List<Participante> listarEventos(){
+        return participanteDAO.listarTodos();
+    }
+    
+    //buscar a un participante por correo
+    public Participante buscarParticipante(String correo){
+        if (correo==null || correo.isEmpty()) {
+            System.out.println("Debe ingresar un correo para buscar");
+            return null;
+        }
+        return participanteDAO.buscarPorCorreo(correo);
+    }
+    
+    //actualizacion de participantes
+    public boolean actualizarParticipante(String correo, String nombreCompleto, String tipoParticipante, String institucion){
+        if (buscarParticipante(correo)== null) {
+            System.out.println("No se puede actualizar o el participante no esta registrado");
+            return false;
+        }
+        Participante participante = new Participante (correo, nombreCompleto, tipoParticipante, institucion);
+        return participanteDAO.actualizar(participante);
+    }
+    
+    //elimunar a un participante
+    public boolean eliminarEvento(String correo){
+        if (buscarParticipante(correo)== null) {
+            System.out.println("No se puede eliminar o el participante no esta registrado");
+            return false;
+        }
+        return participanteDAO.eliminar(correo);
+    }
+}
