ad4a78cd84ebca6f97edffd95d444a44f7f996ae
RonaldSamayoa <RonaldSamayoa@github.com>
2025-08-17 20:31:02 -0600

    cambios en pom para jcalendar y crear jar

--- src/main/java/com/mycompany/eventoshyrule/vista/FrmEvento.java
+++ src/main/java/com/mycompany/eventoshyrule/vista/FrmEvento.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.eventoshyrule.vista;
-
+import com.mycompany.eventoshyrule.controlador.EventoControlador;
+import javax.swing.*;
+import java.awt.*;
 /**
  *
  * @author ronald
  */
-public class FrmEvento {
+public class FrmEvento extends JInternalFrame{
+    private JTextField txtCodigo;
+    private JTextField txtFecha;
+    private JComboBox<String> cbTipoEvento;
+    private JTextField txtTitulo;
+    private JTextField txtUbicacion;
+    private JTextField txtCupoMaximo;
+    private JTextField txtCostoInscripcion;
+    private JButton btnGuardar;
+    private JButton btnCancelar;
     
+    private EventoControlador eventoCtrl;
+    
+    public FrmEvento(){
+        setTitle("Gestion de eventos");
+        setSize(450,350);
+        setClosable(true);
+        setIconifiable(true);
+        setResizable(false);
+        setLayout(new BorderLayout());
+        
+        eventoCtrl = new EventoControlador();
+        
+        //panel de formulario
+        JPanel panelForm = new JPanel(new GridLayout(7, 2, 10, 10));
+        panelForm.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        
+        //campos
+        panelForm.add(new JLabel ("*  Codigo: "));
+        txtCodigo = new JTextField();
+        panelForm.add(txtCodigo);
+    }
 }
